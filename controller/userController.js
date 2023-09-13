e6ac0356f7b0983c5e90cdb6179194264ec3c385
sheraz <sherazzafar148@gmail.com>
2023-09-13 14:43:54 +0500

    crud

--- controller/userController.js
+++ controller/userController.js
@@ -20,3 +20,40 @@ exports.createUser= async (req,res)=>{
         console.log(error)
     }
 }
+
+exports.getAllUser = async (req,res)=>{
+    try{
+        const user = await User.find()
+        return res.status(200).json(user)
+    }catch(e)
+    {
+        console.log(e)
+    }
+}
+exports.updateUser = async(req,res)=>{
+  
+    try{
+        const {userId} = req.params
+        const {name,password,email} = req.body;
+
+        const user = await User.findByIdAndUpdate(userId)
+        if(!user){
+            return res.status(401).json('no such user')
+        }
+        user.name = name
+        user.email = email
+        user.password = password
+        user.save()
+        res.status(200).json(user)
+    }catch(e){console.log(e)}
+}
+exports.deleteUser = async(req,res)=>{
+    try{
+        const {userId} = req.params
+        const user = await User.findByIdAndDelete(userId)
+        if(!user){
+            return res.status(401).json('no such user')
+        }
+        res.status(200).json('user deleted')
+    }catch(e){console.log(e)}
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,15 @@
-const {createUser} = require('../controller/userController')
-const express = require('express')
-const router = express.Router()
+const {createUser,getAllUser,updateUser,deleteUser} = require('../controller/userController')
+const router = require('express').Router()
 router.post('/createUser',createUser)
+router.get('/getAllUser',getAllUser)
+router.put('/update/:userId',updateUser)
+router.delete('/delete/:userId',deleteUser)
+
+
+
+
+
+
+
 
 module.exports = router
